90ad032d57a788589d7acd5a50f9398b488bfe9c
lenonbr <lenon.br@hotmail.com>
2025-06-18 15:23:11 -0300

    ÔÜí perf: adiciona valor em onda-components

---
+++ src/financeiro/formulario/FinanceiroFormularioRecebedor/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function FinanceiroFormularioRecebedor() {
+    return <button className="bg-blue-500 text-white p-2 rounded"></button>;
+}
+
+export default FinanceiroFormularioRecebedor;
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,3 @@
-export {default as Button} from "./components/Button";
+// export {default as Button} from "./components/Button";
+// export * from "../dist/index";
+export * from "../src";
