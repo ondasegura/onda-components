8d5aee29afaa2f23847d5e74d159f1ed52c10c6c
lenonbr <lenon.br@hotmail.com>
2025-06-18 11:47:06 -0300

    ÔÜí perf: adiciona valor em onda-components

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -3,12 +3,13 @@ import {pluginReact} from "@rsbuild/plugin-react";
 
 export default defineConfig({
     plugins: [pluginReact()],
+    source: {
+        entry: {
+            index: "./src/index.ts",
+        },
+    },
     output: {
         distPath: {root: "./dist"},
         cleanDistPath: true,
-        filename: {
-            js: "index.js",
-        },
-        assetPrefix: "./",
     },
 });
