eac2601e9e7ea2fd82c8ac17cf6db5330757d54d
lenonbr <lenon.br@hotmail.com>
2025-06-18 17:08:15 -0300

    ÔÜí perf: adiciona valor em onda-components

---
+++ dist/financeiro/componentes/geral/Button.d.ts
@@ -0,0 +1,5 @@
+import React from "react";
+declare const Button: React.FC<{
+    children: React.ReactNode;
+}>;
+export default Button;
---
+++ dist/index.d.ts
@@ -0,0 +1,3 @@
+import controller_conta_pagar from "./financeiro/controllers/contas_pagar";
+export { default as FormularioFinanceiroContaPagar } from "./financeiro/componentes/formulario/FinanceiroFormularioContaPagar";
+export default controller_conta_pagar;
