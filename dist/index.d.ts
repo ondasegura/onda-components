76b3858ade9a9c91174983f819fe78c8597713a9
lenonbr <lenon.br@hotmail.com>
2025-06-20 11:00:29 -0300

    ÔÜí perf: adiciona valor em onda-components

--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-import controller_conta_pagar from "./financeiro/controllers/contas_pagar";
-export { default as FormularioFinanceiroContaPagar } from "./financeiro/componentes/formulario/FinanceiroFormularioContaPagar";
-export default controller_conta_pagar;
